e6597018a9625c9499f6395c877a48c4b94cc403
HyeSeong123 <banggu1997@gmail.com>
2020-12-19 16:10:00 +0900

    배포

--- javascript.js
+++ javascript.js
@@ -1,17 +1,22 @@
-/**
- * 
- */
 function EditorViewer1__init() {
-	var bodyEl = document.querySelector('.ed');
-	var initialValue = bodyEl.innerHTML.trim();
-	
+	var body = document.querySelector('.viewer');
+	var initialValue = body.innerHTML.trim();
 	var viewer = new toastui.Editor.factory({
-		el: bodyEl,
-		initialValue: initialValue,
-		viewer: true,
-		
+		el : body,
+		initialValue : initialValue,
+		viewer : true,
+		plugins: [toastui.Editor.plugin.codeSyntaxHighlight]
 	});
-	
 }
-
+function EditorViewer2__init() {
+	var body = document.querySelector('.content');
+	var initialValue = body.innerHTML.trim();
+	var viewer = new toastui.Editor.factory({
+		el : body,
+		initialValue : initialValue,
+		viewer : true,
+		plugins: [toastui.Editor.plugin.codeSyntaxHighlight]
+	});
+}
 EditorViewer1__init();
+EditorViewer2__init();
