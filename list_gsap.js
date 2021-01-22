f955464e3ec6efaaa72d96f15514ffad2867a871
HyeSeong123 <banggu1997@gmail.com>
2021-01-23 01:22:05 +0900

    구글 광고

--- list_gsap.js
+++ list_gsap.js
@@ -1,128 +1,23 @@
 gsap.registerPlugin(ScrollTrigger);
-var tl1 = gsap.timeline();
 
-tl1.to('.balloon1', {
-	x: 600,
-	opacity: 1,
-	duration: 1,
-	ease : "none"
-},"+=0.2");
+function applyGsapToSearchResult() {
 
-tl1.to('.balloon2', {
-	x: -600,
-	opacity: 1,
-	duration: 1,
-	ease : "none"
-});
-
-gsap.to('.balloon3', {
-	scrollTrigger: {
-		trigger: ".balloon2",
-		markers: false,
-		start: "top 15%",
-		end: "bottom 10%",
-		toggleActions: "play none reset none"
-	},
-	x: 600,
-	ease: "none",
-	opacity: 1,
-	duration: 1
-})
-
-gsap.to('.balloon4', {
-	scrollTrigger: {
-		trigger: ".balloon2",
-		markers: false,
-		start: "top 15%",
-		end: "bottom 10%",
-		toggleActions: "play none reset none"
-	},
-	x: -600,
-	ease: "none",
-	opacity: 1,
-	duration: 1
-});
-
-gsap.to('.balloon5', {
-	scrollTrigger: {
-		trigger: ".balloon4",
-		markers: false,
-		start:  "top 15%",
-		end: "bottom 10%",
-		toggleActions: "play none none none"
-	},
-	x: 600,
-	ease: "none",
-	opacity: 1,
-	duration: 1
-})
-
-gsap.to('.balloon6', {
-	scrollTrigger: {
-		trigger: ".balloon4",
-		markers: false,
-		start: "top 15%",
-		end: "bottom 10%",
-		toggleActions: "play none none none"
-	},
-	x: -600,
-	ease: "none",
-	opacity: 1,
-	duration: 1
-})
-
-gsap.to('.balloon7', {
-	scrollTrigger: {
-		trigger: ".balloon6",
-		markers: false,
-		start: "top 15%",
-		end: "bottom 10%",
-		toggleActions: "play none reset none"
-	},
-	x: 600,
-	ease: "none",
-	opacity: 1,
-	duration: 1
-})
-
-gsap.to('.balloon8', {
-	scrollTrigger: {
-		trigger: ".balloon6",
-		markers: false,
-		start: "top 15%",
-		end: "bottom 10%",
-		toggleActions: "play none reset none"
-	},
-	x: -600,
-	ease: "none",
-	opacity: 1,
-	duration: 1
-})
-
-gsap.to('.balloon9', {
-	scrollTrigger: {
-		trigger: ".balloon8",
-		markers: false,
-		start: "top 15%",
-		end: "bottom 10%",
-		toggleActions: "play none reset none"
-	},
-	x: 600,
-	ease: "none",
-	opacity: 1,
-	duration: 1
-})
-
-gsap.to('.balloon10', {
-	scrollTrigger: {
-		trigger: ".balloon8",
-		markers: false,
-		start: "top 15%",
-		end: "bottom 10%",
-		toggleActions: "play none reset none"
-	},
-	x: -600,
-	ease: "none",
-	opacity: 1,
-	duration: 1
-})
+	$('.balloon__a').each(function(index, node) {
+		const gsapSetDone = $(node).data('gsap-set-done');
+		if (gsapSetDone !== true) {
+			gsap.to(node, {
+				scrollTrigger: {
+					trigger: node,
+					markers: false,
+					start: "-370px 25%",
+					end: "-170px 20%",
+				},
+				x: 750,
+				opacity: 1,
+				duration: 1
+			});
+			$(node).data('gsap-set-done', true);
+		}
+	});
+}
+applyGsapToSearchResult();
